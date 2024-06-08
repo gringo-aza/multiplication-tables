70049da6f14af0102926dd1776778f4a94bcad0c
Aziz Rozmetov <argringo97@gmail.com>
2024-06-08 19:56:35 +0500

    Add Multiplication Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+
+Console.Write("Enter the first range: ");
+short firstArgument = Convert.ToInt16(Console.ReadLine());
+Console.Write("Enter the first range: ");
+short lastArgument = Convert.ToInt16(Console.ReadLine());
+
+for(int i = 1; i <= 9; i++)
+{
+    for(int j = firstArgument; j <= lastArgument; j++)
+    {
+        Console.WriteLine($"{j}*{i}={j*i}");
+    }
+    Console.WriteLine();
+}
